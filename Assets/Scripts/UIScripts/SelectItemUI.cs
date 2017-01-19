b08dd733afd43c43aec5b3b40e4200ed5a8b50ff
Ryan Andersen <randersen3@gatech.edu>
2017-01-19 01:17:48 -0500

    added states and ability to select and cancel

---
+++ Assets/Scripts/UIScripts/SelectItemUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectItemUI : MonoBehaviour {
+    int currentActionState;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+        if (Input.GetButtonDown("Select"))
+        {
+            anim.SetInteger("State", currentActionState);
+            anim.ResetTrigger("Cancel");
+            anim.SetTrigger("Select");
+
+        }
+        else if (Input.GetButtonDown("Cancel"))
+        {
+            anim.SetInteger("State", currentActionState);
+            anim.ResetTrigger("Select");
+            anim.SetTrigger("Cancel");
+        }
+	}
+}
