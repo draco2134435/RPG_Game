870353f0c6ef82040278641c7b3f018c10d96871
Ryan Andersen <randersen3@gatech.edu>
2017-01-22 15:36:18 -0500

    camera follow

---
+++ Assets/Scripts/CameraMechanics/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    public float xOffset = 0;
+    public float yOffset = 0;
+    public float movementSpeed = 50;
+    Transform targetObject;
+
+
+    void Start()
+    {
+        targetObject = transform.parent;
+        transform.parent = null;
+        xOffset = transform.position.x - targetObject.position.x;
+        yOffset = transform.position.y - targetObject.position.y;
+    }
+
+    void Update()
+    {
+        Vector3 goalPosition = new Vector3(targetObject.position.x + xOffset, targetObject.position.y + yOffset, transform.position.z);
+        transform.position = Vector3.Slerp(transform.position, goalPosition, Time.deltaTime * movementSpeed);
+    }
+}
