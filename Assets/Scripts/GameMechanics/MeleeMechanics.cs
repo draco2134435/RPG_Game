fb23c9cabb2a7e1eac18cd35fe0c5a2ffa38d713
Ryan Andersen <randersen3@gatech.edu>
2017-01-23 19:32:36 -0500

    melee animations added

---
+++ Assets/Scripts/GameMechanics/MeleeMechanics.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeMechanics : MonoBehaviour {
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        attack(Input.GetButtonDown("Melee"));
+    }
+
+    public void attack(bool attackInput)
+    {
+        if (attackInput)
+        {
+            anim.SetTrigger("Melee");
+        }
+    }
+}
