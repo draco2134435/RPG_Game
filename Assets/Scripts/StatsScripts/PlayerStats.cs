18d2ca5d952d556cf13024ac3f6907188594a199
Ryan Andersen <randersen3@gatech.edu>
2017-01-24 22:45:15 -0500

    Player stats added and some animations started

---
+++ Assets/Scripts/StatsScripts/PlayerStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    /// <summary>
+    /// Player stats contains the scales for different stats in of the player.
+    /// Base stats have permanent changes, curr stats are for temporary changes.
+    /// </summary>
+    public float baseHp = 1;
+    public float currHp = 1;
+
+
+    public float baseStr = 1;
+    public float currStr = 1;
+
+
+    public float baseSpd = 1;
+    public float currSpd = 1;
+}
