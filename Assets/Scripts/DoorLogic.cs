cbdb916b74111f4fedd73f3f9e835916367fa77e
Ryan Andersen <randersen3@gatech.edu>
2017-02-04 23:37:13 -0500

    Basic Door mechanics added

--- Assets/Scripts/DoorLogic.cs
+++ Assets/Scripts/DoorLogic.cs
@@ -2,16 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Collider2D))]
+
 public class DoorLogic : MonoBehaviour {
     public DoorLogic destinationDoor;
+    public Transform playerSpawnPoint;
+    Animator anim;
     public bool isLocked = false;
+    public bool canOpen = false;
+    GameObject player;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (canOpen && Input.GetButtonDown("Action"))
+        {
+            transportPlayer();
+        }
+    }
 
 
-    void OnColliderEnter2D(Collider2D collider)
+    public void transportPlayer()
+    {
+        player.transform.position = destinationDoor.playerSpawnPoint.position;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            canOpen = true;
+            player = collider.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
     {
         if (collider.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-
+            canOpen = false;
         }
     }
 }
